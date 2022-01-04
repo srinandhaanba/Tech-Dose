6a055447d9a13d05a01971ae5d6cfaf90ba4a2a6
srinandhaanba <57598534+srinandhaanba@users.noreply.github.com>
2022-01-04 13:30:27 +0530

    Update isprime.java

--- Math Algorithm/isprime.java
+++ Math Algorithm/isprime.java
@@ -1,1 +1,13 @@
 
+class Main{
+    public static void main(String[] args) {
+        int n=105;
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if (n%i==0){
+                System.out.println("Not a prime");
+                return;
+            }
+        }
+        System.out.println("It is a prime");
+    }
+}
