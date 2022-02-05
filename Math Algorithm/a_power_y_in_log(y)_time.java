cd444cb3bcc82df3d50530801c91206b9b6efd9f
srinandhaanba <57598534+srinandhaanba@users.noreply.github.com>
2022-02-05 19:47:00 +0530

    Update a_power_y_in_log(y)_time.java

--- Math Algorithm/a_power_y_in_log(y)_time.java
+++ Math Algorithm/a_power_y_in_log(y)_time.java
@@ -1,1 +1,20 @@
+import java.util.*;
+public class Hello {
 
+    public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt(),b=sc.nextInt();
+		long res=1;
+		while(b>0)
+        {
+            if(b%2==1)
+            {
+                res*=a;
+            }
+            a*=a;
+            b>>=1;
+        }
+        System.out.print(res);
+
+	}
+}
