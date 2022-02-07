bf5b7633e71a15a2287aed2c725f0cd0193d8650
srinandhaanba <57598534+srinandhaanba@users.noreply.github.com>
2022-02-07 20:19:52 +0530

    Update Find_All_factor_of_N.c

--- Math Algorithm/Find_All_factor_of_N.c
+++ Math Algorithm/Find_All_factor_of_N.c
@@ -1,1 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    for(i=1;i*i<n;i++){
+        if(n%i==0)
+        {
+          printf("%d %d ",i,n/i);
+        }
+    }
+    if(i*i==n)printf("%d",i);
+}
