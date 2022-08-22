2cb3bf2ee9f956c753eddb3f6be2bf490d074d4b
srinandhaanba <57598534+srinandhaanba@users.noreply.github.com>
2022-08-22 20:32:20 +0530

    Update Insertion_Sort.java

--- sort/Insertion_Sort.java
+++ sort/Insertion_Sort.java
@@ -1,1 +1,25 @@
 
+public class insertionSort {
+    public static void InsertionSort(int[] arr,int n)
+    {
+        for(int i=1;i<n;i++)
+        {
+            int j=i-1,x=arr[i];
+            while(j>-1 && arr[j]>x)
+            {
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=x;
+            
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr={6,7,3,4,9,1,4};
+        InsertionSort(arr,arr.length);
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
